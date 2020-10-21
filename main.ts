e0a481f9ff67214ff9577c6ada6a56de6eadc3ee
Esther-147 <71751338+Esther-147@users.noreply.github.com>
2020-10-21 15:22:49 +0800

    one

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
-input.onButtonPressed(Button.A, function () {
-    radio.sendString("oh my god")
-})
-radio.onReceivedString(function (receivedString) {
-    basic.showString("uuuuuuu")
-})
-basic.showNumber(0)
-radio.setGroup(1)
 basic.forever(function () {
-	
+    for (let I = 0; I <= 4; I++) {
+        for (let j = 0; j <= 4; j++) {
+            if (I % 2 == j % 2) {
+                led.plot(I, j)
+            }
+        }
+    }
 })
