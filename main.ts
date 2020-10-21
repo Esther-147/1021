dd77e714c2cf446a1cb3c343d432be20c8bce705
Esther-147 <71751338+Esther-147@users.noreply.github.com>
2020-10-21 15:40:01 +0800

    1

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
     for (let I = 0; I <= 4; I++) {
         for (let j = 0; j <= 4; j++) {
-            if (I % 2 == j % 2) {
+            if (I % 2 != j % 2) {
                 led.plot(I, j)
             }
         }
