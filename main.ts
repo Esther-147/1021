2abcbe8fcac3be202d9f1aa4dd61028430f06dc9
Esther-147 <71751338+Esther-147@users.noreply.github.com>
2020-10-21 15:41:21 +0800

    HOMEWORK1

--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
 basic.forever(function () {
     for (let I = 0; I <= 4; I++) {
         for (let j = 0; j <= 4; j++) {
-            if (I % 2 == 0 && j % 2 == 0) {
-                led.plot(I, j)
-            } else if (I % 2 != 0 && j % 2 != 0) {
-                led.plot(I, j)
+            if (true) {
+            	
             }
         }
     }
